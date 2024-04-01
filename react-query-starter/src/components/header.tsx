adcd9e0a3948a8cd71e9dd1291a50e1d2680633d
hoapng <giahoa01giathieu@gmail.com>
2024-04-01 13:09:24 +0700

    Sharing Data Between Components

--- react-query-starter/src/components/header.tsx
+++ react-query-starter/src/components/header.tsx
@@ -1,42 +1,55 @@
-
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import { useEffect, useState } from 'react';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function Header() {
+  const [mode, setMode] = useState("light");
+  const [count, setCount] = useState<number>(0);
 
-    const [mode, setMode] = useState("light")
+  const { data } = useQuery({
+    queryKey: ["fetchUser", 1],
+    queryFn: (): Promise<any[]> =>
+      fetch(`http://localhost:8000/users?_page=${1}&_limit=${2}`).then(
+        (res) => {
+          const total_items = +(res.headers?.get("X-Total-Count") ?? 0);
+          setCount(total_items);
+          return res.json();
+        }
+      ),
+  });
 
-    useEffect(() => {
-        const body = document.querySelector("body");
-        if (body) body.setAttribute('data-bs-theme', mode);
-    }, [mode])
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body) body.setAttribute("data-bs-theme", mode);
+  }, [mode]);
 
-
-    return (
-        <Navbar className="bg-body-tertiary" data-bs-theme={mode}>
-            <Container>
-                <Navbar.Brand href="#home">Hỏi Dân IT React Query</Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Form.Check
-                        defaultChecked={mode === "light" ? false : true}
-                        onChange={(e) => {
-                            setMode(e.target.checked === true ? "dark" : "light")
-                        }}
-                        type="switch"
-                        id="custom-switch"
-                        label={mode === "light" ?
-                            <Navbar.Text>Light mode</Navbar.Text>
-                            :
-                            <Navbar.Text>Dark mode</Navbar.Text>
-                        }
-                    />
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar className="bg-body-tertiary" data-bs-theme={mode}>
+      <Container>
+        <Navbar.Brand href="#home">React Query {count}</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Form.Check
+            defaultChecked={mode === "light" ? false : true}
+            onChange={(e) => {
+              setMode(e.target.checked === true ? "dark" : "light");
+            }}
+            type="switch"
+            id="custom-switch"
+            label={
+              mode === "light" ? (
+                <Navbar.Text>Light mode</Navbar.Text>
+              ) : (
+                <Navbar.Text>Dark mode</Navbar.Text>
+              )
+            }
+          />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
